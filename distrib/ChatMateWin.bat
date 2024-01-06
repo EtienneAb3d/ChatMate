f01596c4a2b47a63b8067315aecf20f1257b54db
Win10 <Win10@DESKTOP-KCAHJ6B>
2024-01-06 10:17:02 +0100

    Enforce UTF-8 on Windows

--- distrib/ChatMateWin.bat
+++ distrib/ChatMateWin.bat
@@ -1,1 +1,1 @@
-java -cp ChatMate.jar;json-simple-1.1.1.jar;swt-win.jar com.cubAIx.ChatMate.ChateMateGui
+java -Dfile.encoding=UTF-8 -cp ChatMate.jar;json-simple-1.1.1.jar;swt-win.jar com.cubAIx.ChatMate.ChateMateGui
